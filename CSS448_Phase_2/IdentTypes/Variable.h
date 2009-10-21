ce2ba00c2787e37749a93be90ca8ef614bdfd9e6
Michael Xavier <michael@michaelxavier.net>
2009-10-20 23:01:04 -0700

    added real and placeholder data members

--- CSS448_Phase_2/IdentTypes/Variable.h
+++ CSS448_Phase_2/IdentTypes/Variable.h
@@ -7,5 +7,8 @@ class Variable : public IdentRecord {
 public:
 
 private:
+  //TODO: figure out how we want to store type
+  IdentRecord* value;//FIXME: not sure if this is how we will implement it
+  string name;//FIXME: not sure if we should use a string or char* or something else
 }
 #endif
