9b3f152a52a2b0fd3ed2dd6515702e4d93abf401
Michael Xavier <michael@michaelxavier.net>
2009-11-18 18:41:43 -0800

    fixed a few probs with makefile. merging to master if it wokrs

--- CSS448_Phase_3/actions.cpp
+++ CSS448_Phase_3/actions.cpp
@@ -3,6 +3,7 @@
 void startProgram(const string& name) {
   Program* newProg = new Program(name);
   symTable = new STObject(newProg);   // ST is created, Program inserted into ST*/
+  cout << "Built a program" << endl;//DEBUG
 }
 
 void endProgram(void) {
